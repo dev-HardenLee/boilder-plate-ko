8fcb3f55c157400fa9f5dbe3656266ce39062bdd
harden <harden.lee@companywe.co.kr>
2024-02-02 06:38:30 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+const config = require('./config/key')
+
+// aplication/x-www-roem-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// aplication/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://harden:harden@cluster0.pafeuqy.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! ~ 안녕하세요')
+  res.send('Hello World! ~ 안녕하세요. !')
+})
+
+app.post('/register', async (req, res) => {
+    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+    } catch (err) {
+        return res.json({
+            success: false, 
+            err
+        })
+    }
+
+    return res.status(200).json({
+        success: true
+    })
 })
 
 app.listen(port, () => {
